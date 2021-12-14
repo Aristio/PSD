0cef448268e5cd302a2aa7c14cd4bef0c8c75dea
LuqmanAristio <96041357+LuqmanAristio@users.noreply.github.com>
2021-12-14 23:40:41 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int session=-1;
+
+struct node{
+	int nim;
+	char nama[100];
+	char kelamin[10];
+	char tgl_lahir[100];
+	char tempat_lahir[100];
+	char alamat[100];
+	char kelas;
+	float UAS, UTS, quiz, tgs, rata;
+	char grade;
+
+	struct node *next;
+	struct node *prev;
+};
